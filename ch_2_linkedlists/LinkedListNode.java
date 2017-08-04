a010ad3f25c54327525652c126c28ed1225b037a
Bailey Phan <bailey.phan@uky.edu>
2017-08-03 23:28:28 -0700

    Add LinkedList directory and LinkedListNode class.

---
+++ ch_2_linkedlists/LinkedListNode.java
@@ -0,0 +1,12 @@
+package ch_2_linkedlists;
+
+public class LinkedListNode {
+	
+	public String data;
+	public LinkedListNode next;
+	
+	public LinkedListNode(String myData, LinkedListNode myNext){
+		data = myData;
+		next = myNext;
+	}
+}
